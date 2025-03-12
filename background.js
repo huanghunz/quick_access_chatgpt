148af5eae18d57b05292551d392563f7e1b73833
huanghunz <huanghunz@hotmail.com>
2025-03-12 09:46:55 -0700

    initial push: context menu option to open chatgpt window

---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "open-chatgpt",
+    title: "Ask ChatGPT",
+    contexts: ["all"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "open-chatgpt") {
+    chrome.windows.create({
+      url: 'https://chat.openai.com',
+      type: 'popup',
+      width: 400,
+      height: 600,
+      left: 100,
+      top: 100
+    });
+  }
+});
